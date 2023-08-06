e97d70940230bae4e48c07da1541647c40768543
Tyler Mommsen <65496518+TylerMommsen@users.noreply.github.com>
2023-08-06 21:59:32 +0300

    Added function to update score when user or pc lines up 3 markers.

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-const player = (name) => {
-    let score = 0;
-    const increaseScore = () => score++;
-    return { name, score, increaseScore };
+const scores = { user: 0, computer: 0 };
+
+const player = (name, scores) => {
+    const increaseScore = () => {
+        scores[name]++;
+    }
+    return { name, increaseScore };
 }
+
 
 const gameBoard = (() => {
     let board =
         [null, null, null,
         null, null, null,
-            null, null, null];
+        null, null, null];
     
     const checkPosition = (index) => {
         if (board[index] === null) return true;
@@ -17,49 +21,93 @@ const gameBoard = (() => {
     return { board, checkPosition };
 })();
 
+
 const displayController = (() => {
     const boardSquares = document.querySelectorAll('.square');
+    const userScore = document.querySelector('#user');
+    const computerScore = document.querySelector('#computer');
 
     boardSquares.forEach(function (square, index) {
         square.addEventListener('click', () => {
             let current = gameController.handleSquareClick(index);
-            if (current === 'User') {
+            if (current === 'user') {
                 square.textContent = 'X';
-            } else if (current === 'Computer') {
+            } else if (current === 'computer') {
                 square.textContent = 'O';
             }
         })
     })
 
-    return { boardSquares };
+    const updateScore = (player) => {
+        if (player.name === 'user') {
+            userScore.textContent = `User: ${scores.user}`;
+        } else {
+            computerScore.textContent = `Computer: ${scores.computer}`;
+        }
+    }
+
+    return { boardSquares, updateScore };
 })();
 
+
 const gameController = (() => {
-    const user = player('User');
-    const computer = player('Computer');
+    const user = player('user', scores);
+    const computer = player('computer', scores);
 
     let currentTurn = user;
 
-    const checkWinner = () => {
+    const checkGameWinner = () => {
         if (currentTurn.score === 3) {
-            currentTurn.increaseScore();
+            console.log('winner is ' + currentTurn.name + '!');
             return currentTurn;
         }
     }
 
+    const checkRoundWinner = () => {
+        let marker = null;
+        if (currentTurn === user) {
+            marker = 'X';
+        } else {
+            marker = 'O';
+        }
+
+        let board = gameBoard.board;
+        if (
+            (board[0] === marker && board[1] === marker && board[2] === marker) || //top-across
+            (board[0] === marker && board[3] === marker && board[6] === marker) || //left-down
+            (board[1] === marker && board[4] === marker && board[7] === marker) || //middle-down
+            (board[2] === marker && board[5] === marker && board[8] === marker) || //right-down
+            (board[3] === marker && board[4] === marker && board[5] === marker) || //middle-across
+            (board[6] === marker && board[7] === marker && board[8] === marker) || //bottom-across
+            (board[0] === marker && board[4] === marker && board[8] === marker) || //left-diagonal
+            (board[2] === marker && board[4] === marker && board[6] === marker)) { //right-diagonal
+            return true;
+        } 
+    }
+
     const handleSquareClick = (index) => {
         if (gameBoard.checkPosition(index)) {
-            if (currentTurn.name === 'User') {
+            if (currentTurn.name === 'user') {
                 gameBoard.board[index] = 'X';
+                if (checkRoundWinner()) {
+                    currentTurn.increaseScore();
+                    displayController.updateScore(currentTurn);
+                    checkGameWinner();
+                }
                 currentTurn = computer;
-                return 'User';
+                return 'user';
             } else {
                 gameBoard.board[index] = 'O';
+                if (checkRoundWinner()) {
+                    currentTurn.increaseScore();
+                    displayController.updateScore(currentTurn);
+                    checkGameWinner();
+                }
                 currentTurn = user;
-                return 'Computer';
+                return 'computer';
             }
         }
     }
 
-    return {handleSquareClick}
+    return {handleSquareClick, user}
 })();
