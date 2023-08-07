8b18ca04172bab318e0cda724038f69e349a3bec
Tyler Mommsen <65496518+TylerMommsen@users.noreply.github.com>
2023-08-07 15:22:27 +0300

    Removed score counter, added draw functionality and a restart button when game ends.

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-const scores = { user: 0, computer: 0 };
-
 const player = (name, scores) => {
-    const increaseScore = () => {
-        scores[name]++;
-    }
-    return { name, increaseScore };
+    return { name };
 }
 
 
@@ -24,10 +19,9 @@ const gameBoard = (() => {
 
 const displayController = (() => {
     const boardSquares = document.querySelectorAll('.square');
-    const userScore = document.querySelector('#user');
-    const computerScore = document.querySelector('#computer');
     const winnerDisplay = document.querySelector('.winner-display');
     const darkOverlay = document.querySelector('.dark-overlay');
+    const restartBtn = document.querySelector('#restart');
 
     boardSquares.forEach(function (square, index) {
         square.addEventListener('click', () => {
@@ -40,14 +34,6 @@ const displayController = (() => {
         })
     })
 
-    const updateScore = (player) => {
-        if (player.name === 'user') {
-            userScore.textContent = `User: ${scores.user}`;
-        } else {
-            computerScore.textContent = `Computer: ${scores.computer}`;
-        }
-    }
-
     const resetBoardUI = () => {
         boardSquares.forEach(function (square) {
             square.textContent = '';
@@ -56,48 +42,35 @@ const displayController = (() => {
         if (winnerDisplay.style.display === 'flex') {
             darkOverlay.style.display = 'none';
             winnerDisplay.style.display = 'none';
-            winnerDisplay.textContent = ``;
-            userScore.textContent = 'User: 0';
-            computerScore.textContent = 'Computer: 0';
+            winnerDisplay.firstChild.textContent = ``;
         }
     }
 
     const displayWinner = (winner) => {
         darkOverlay.style.display = 'block';
         winnerDisplay.style.display = 'flex';
-        winnerDisplay.textContent = `${winner.name[0].toUpperCase()}${winner.name.slice(1)} wins!`;
+        if (winner === 'draw') {
+            winnerDisplay.firstChild.textContent = "It's a draw!";
+        } else {
+            winnerDisplay.firstChild.textContent = `${winner.name[0].toUpperCase()}${winner.name.slice(1)} wins!`;
+        }
     }
 
-    return { boardSquares, updateScore, resetBoardUI, displayWinner };
+    restartBtn.addEventListener('click', () => {
+        gameController.resetBoard();
+    })
+
+    return { boardSquares, resetBoardUI, displayWinner };
 })();
 
 
 const gameController = (() => {
-    const user = player('user', scores);
-    const computer = player('computer', scores);
+    const user = player('user');
+    const computer = player('computer');
 
     let currentTurn = user;
 
-    const checkGameWinner = () => {
-        if (scores[currentTurn.name] === 3) {
-            displayController.displayWinner(currentTurn);
-            setTimeout(() => { 
-                displayController.resetBoardUI();
-                scores['user'] = 0;
-                scores['computer'] = 0;
-            }, 3000);
-            return;
-        }
-    }
-
-    const checkRoundWinner = () => {
-        let marker = null;
-        if (currentTurn === user) {
-            marker = 'X';
-        } else {
-            marker = 'O';
-        }
-
+    const checkRoundWinner = (marker) => {
         let board = gameBoard.board;
         if (
             (board[0] === marker && board[1] === marker && board[2] === marker) || //top-across
@@ -112,29 +85,43 @@ const gameController = (() => {
         } 
     }
 
+    const checkDraw = () => {
+        let board = gameBoard.board;
+        let hasSpaces = false;
+        board.forEach(function (square, i) {
+            if (square === null) {
+                hasSpaces = true;
+            } 
+        })
+
+        if (hasSpaces) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     const handleSquareClick = (index) => {
         if (gameBoard.checkPosition(index)) {
             if (currentTurn.name === 'user') {
                 gameBoard.board[index] = 'X';
-                if (checkRoundWinner()) {
-                    currentTurn.increaseScore();
-                    displayController.updateScore(currentTurn);
-                    resetBoard();
-                    checkGameWinner();
-                    return;
+                if (checkRoundWinner('X')) {
+                    displayController.displayWinner(currentTurn);
+                } else if (!checkDraw()) {
+                    displayController.displayWinner('draw');
+                } else {
+                    currentTurn = computer;
                 }
-                currentTurn = computer;
                 return 'user';
             } else {
                 gameBoard.board[index] = 'O';
-                if (checkRoundWinner()) {
-                    currentTurn.increaseScore();
-                    displayController.updateScore(currentTurn);
-                    resetBoard();
-                    checkGameWinner();
-                    return;
+                if (checkRoundWinner('O')) {
+                    displayController.displayWinner(currentTurn);
+                } else if (!checkDraw()) {
+                    displayController.displayWinner('draw');
+                } else {
+                    currentTurn = user;
                 }
-                currentTurn = user;
                 return 'computer';
             }
         }
@@ -142,10 +129,11 @@ const gameController = (() => {
 
     const resetBoard = () => {
         displayController.resetBoardUI();
+        currentTurn = user;
         gameBoard.board.forEach(function (value, i) {
             gameBoard.board[i] = null;
         })
     }
 
-    return {handleSquareClick, user}
+    return {handleSquareClick, resetBoard}
 })();
